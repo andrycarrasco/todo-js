5ac9141f76676a05003ad3fabf735ae106b95c5e
Andry <andryjuca@gmail.com>
2020-11-13 09:09:35 -0300

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//      IMPORTS 
+import './styles.css';
+import { Todo, TodoList } from './classes'
+import { crearHTML } from './js/componentes';
+
+// import { Todo } from './classes/todo.class';
+// import { TodoList } from './classes/todo-list.class';
+
+//      CODE 
+export const todoList = new TodoList();
+
+// todoList.todos.forEach( todo=>crearHTML( todo ) );
+todoList.todos.forEach( crearHTML );
+
+
+
